47fabc35324d8d4cafa644fbfecd906d744730e8
Jari Bou <bourdietome@gmail.com>
2024-02-28 11:27:15 +0100

    Update to Monster so and added Monster Instance and condiments

--- Assets/_project/ScriptableObjects/Scripts/MonsterDataSo.cs
+++ Assets/_project/ScriptableObjects/Scripts/MonsterDataSo.cs
@@ -10,28 +10,29 @@ namespace _project.ScriptableObjects.Scripts
         [SerializeField] private string _name;
         [SerializeField, TextArea] private string _description;
         
-        [SerializeField, TabProperty("Stats Config")] private Vector3 _statsMin;
-        [SerializeField, TabProperty("Stats Config")] private Vector3 _statsMax;
+        [SerializeField, Tooltip("Min stats to have for that stat to win"), TabProperty("Stats Config")] private Vector3Int _statsMin;
+        [SerializeField, Tooltip("Max stats to have for that stat to win"), TabProperty("Stats Config")] private Vector3Int _statsMax;
         [SerializeField, TabProperty("Stats Config")] private IngredientsBundleSo _ingredients;
+        [SerializeField, Tooltip("Min possible random rolled value for a stat"), TabProperty("Stats Config")] private Vector3Int _randomStatsMin;
+        [SerializeField, Tooltip("Max possible random rolled value for a stat"), TabProperty("Stats Config")] private Vector3Int _randomStatsMax;
 
         [SerializeField, TabProperty("Sprites")] private Sprite _icon;
         [SerializeField, TabProperty("Sprites")] private Sprite _sleepingSprite;
         [SerializeField, TabProperty("Sprites")] private Sprite _normalSprite;
         [SerializeField, TabProperty("Sprites")] private Sprite _angrySprite;
-        
-        // TODO: hum.... this probably shouldn't stay here but whatever we'll see later
-        [ShowProperty] public Vector3 CurrentStats { get; set; }
 
         public string Name => _name;
         public string Description => _description;
         
-        public Vector3 StatsMin => _statsMin;
-        public Vector3 StatsMax => _statsMax;
+        public Vector3Int StatsMin => _statsMin;
+        public Vector3Int StatsMax => _statsMax;
         public IngredientsBundleSo Ingredients => _ingredients;
         
         public Sprite Icon => _icon;
         public Sprite SleepingSprite => _sleepingSprite;
         public Sprite NormalSprite => _normalSprite;
         public Sprite AngrySprite => _angrySprite;
+        public Vector3Int RandomStatsMin => _randomStatsMin;
+        public Vector3Int RandomStatsMax => _randomStatsMax;
     }
 }
---
+++ Assets/_project/Scripts/MonsterInstance.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using _project.ScriptableObjects.Scripts;
+using _project.Scripts.Core;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace _project.Scripts
+{
+    public class MonsterInstance
+    {
+        public Vector3 CurrentStats { get; private set; }
+
+        public MonsterDataSo MonsterData { get; private set; }
+
+        public MonsterInstance(MonsterDataSo dataSo)
+        {
+            MonsterData = dataSo;
+
+            int x = Random.Range(dataSo.RandomStatsMin.x, dataSo.RandomStatsMax.x);
+            int y = Random.Range(dataSo.RandomStatsMin.y, dataSo.RandomStatsMax.y);
+            int z = Random.Range(dataSo.RandomStatsMin.z, dataSo.RandomStatsMax.z);
+            CurrentStats = new Vector3(x, y, z);
+        }
+
+        public bool FeedMeal(Meal meal)
+        {
+            CurrentStats += meal.Stats;
+            CurrentStats.Clamp(Vector3.zero, new Vector3(100, 100, 100));
+
+            return CurrentStats.IsInBounds(MonsterData.StatsMin, MonsterData.StatsMax);
+        }
+
+        public List<IngredientSo> GetPossibleIngredients()
+        {
+            return MonsterData.Ingredients.BundleIngredients;
+        }
+
+        public Sprite GetDisplayedSprite()
+        {
+            int xDelta = (int)Math.Abs((MonsterData.StatsMax.x - MonsterData.StatsMin.x) / 2f - CurrentStats.x);
+            int yDelta = (int)Math.Abs((MonsterData.StatsMax.y - MonsterData.StatsMin.y) / 2f - CurrentStats.y);
+            int zDelta = (int)Math.Abs((MonsterData.StatsMax.z - MonsterData.StatsMin.z) / 2f - CurrentStats.z);
+
+            int averageDistance = (xDelta + yDelta + zDelta) / 3;
+            
+            return averageDistance switch
+            {
+                < 50 => MonsterData.SleepingSprite,
+                > 100 => MonsterData.AngrySprite,
+                _ => MonsterData.NormalSprite
+            };
+        }
+    }
+}
