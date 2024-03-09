efa24437f3b5abf251f8c8ca35f2e501ea0593c6
Jari Bou <bourdietome@gmail.com>
2024-03-09 15:18:11 +0100

    trying to make login work

--- Assets/GooglePlayServices.cs
+++ Assets/GooglePlayServices.cs
@@ -3,20 +3,36 @@ using System.Collections.Generic;
 using GooglePlayGames;
 using GooglePlayGames.BasicApi;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class GooglePlayServices : MonoBehaviour
 {
+    private string _token;
 
-    public void Start() {
+    public string Token => _token;
+
+    public void Awake()
+    {
+      PlayGamesPlatform.Activate();
       PlayGamesPlatform.Instance.Authenticate(ProcessAuthentication);
       PlayGamesPlatform.Instance.ReportProgress("CgkI_ND8rucOEAIQAQ", 100.0f, (bool success) => {
         // handle success or failure
       });
     }
 
-    internal void ProcessAuthentication(SignInStatus status) {
-      if (status == SignInStatus.Success) {
+    public void ShowAchievements()
+    {
+      PlayGamesPlatform.Instance.ShowAchievementsUI();
+    }
+
+    private void ProcessAuthentication(SignInStatus status) {
+      if (status == SignInStatus.Success)
+      {
         // Continue with Play Games Services
+        PlayGamesPlatform.Instance.RequestServerSideAccess(true, code =>
+        {
+          _token = code;
+        });
       } else {
         // Disable your integration with Play Games Services or show a login button
         // to ask users to sign-in. Clicking it should call
