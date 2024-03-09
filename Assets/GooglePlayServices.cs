f54596bf08807d27431360a40079582a67624a69
Jari Bou <bourdietome@gmail.com>
2024-03-09 17:58:05 +0100

    debug stuff

--- Assets/GooglePlayServices.cs
+++ Assets/GooglePlayServices.cs
@@ -4,18 +4,26 @@ using GooglePlayGames;
 using GooglePlayGames.BasicApi;
 using UnityEngine;
 using UnityEngine.Serialization;
+using UnityEngine.UI;
 
 public class GooglePlayServices : MonoBehaviour
 {
     private string _token;
+    [SerializeField] private GameObject _manualConnectButton;
+    [SerializeField] private GameObject _achievementsButton;
+    [SerializeField] private Image _statusImage;
+    [SerializeField] private Slider _progressDebugSlider;
 
     public string Token => _token;
 
-    public void Awake()
+    public void Start()
     {
       PlayGamesPlatform.Activate();
+      _progressDebugSlider.value = 1 / 3f;
       PlayGamesPlatform.Instance.Authenticate(ProcessAuthentication);
+      _progressDebugSlider.value = 2 / 3f;
       PlayGamesPlatform.Instance.ReportProgress("CgkI_ND8rucOEAIQAQ", 100.0f, (bool success) => {
+      _progressDebugSlider.value = 3 / 3f;
         // handle success or failure
       });
     }
@@ -33,11 +41,21 @@ public class GooglePlayServices : MonoBehaviour
         {
           _token = code;
         });
+        _manualConnectButton.SetActive(false);
+        _achievementsButton.SetActive(true);
+        _statusImage.color = Color.green;
       } else {
         // Disable your integration with Play Games Services or show a login button
         // to ask users to sign-in. Clicking it should call
         // PlayGamesPlatform.Instance.ManuallyAuthenticate(ProcessAuthentication).
+        _manualConnectButton.SetActive(true);
+        _statusImage.color = Color.red;
       }
+    }
+
+    public void ManualConnection()
+    {
+      PlayGamesPlatform.Instance.ManuallyAuthenticate(ProcessAuthentication);
     }
 
 }
