bb9884ff6137e9ce894f0ddf5f1ad5bff25d99ff
Shahine <shahine.benthanane@edu.devinci.fr>
2024-03-13 12:52:56 +0100

    C'est fix pour la build

--- Assets/_project/Scripts/UI/Option Menu.cs
+++ Assets/_project/Scripts/UI/Option Menu.cs
@@ -95,7 +95,6 @@ namespace _project.Scripts.UI
         {
             _optionPanel.SetActive(false);
             _isOptionPanelOpen = false;
-            _settingsButton.raycastTarget = true;
         }
 
         public void OpenSettingsPanel()
